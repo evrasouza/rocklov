16643d664edb18a3ee888c6d5d48a25e5b5a289b
Everton Souza <evrasouza@gmail.com>
2021-03-25 19:58:25 -0300

    Formulário de Login

--- features/step_definitions/cadastro_steps.rb
+++ features/step_definitions/cadastro_steps.rb
@@ -15,10 +15,6 @@ Quando('submeto o seguinte forulário de cadastro:') do |table|
     click_button "Cadastrar"
 
 end                                                                           
-  
-Então('sou redirecionado para o Dashboard') do
-    expect(page).to have_css ".dashboard"
-end
 
 Então('vejo a mensagem de alerta: {string}') do |expect_alert|
     alert = find(".alert-dark")
