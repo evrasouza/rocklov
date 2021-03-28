b90b5371025fb5ba8d7517b0c9c25e35e95d02ee
Everton Souza <evrasouza@gmail.com>
2021-03-28 19:59:41 -0300

    Refatorando o Cadastro de Usuários

--- features/step_definitions/cadastro_steps.rb
+++ features/step_definitions/cadastro_steps.rb
@@ -1,17 +1,9 @@
 Dado('que acesso a página de cadastro') do
-    visit "/signup"
+    @SignUpPage.open
 end
   
 Quando('submeto o seguinte forulário de cadastro:') do |table|
-
     user = table.hashes.first
-
     MongoDB.new.remove_user(user[:email])
-
-    find("#fullName").set user[:nome]
-    find("#email").set user[:email]
-    find("#password").set user[:senha]
-
-    click_button "Cadastrar"
-
+    @signup_page.create(user)
 end
