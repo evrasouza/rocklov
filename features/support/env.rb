1cf73bd275196a8b08eeb9fc4d20dc1ab161b35e
Everton Souza <evrasouza@gmail.com>
2021-03-29 19:53:39 -0300

    Instalando Rufo para identar o codigo em Ruby

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,12 +1,12 @@
-require 'webdrivers'
+require "webdrivers"
 require "selenium-webdriver"
 require "capybara"
 require "capybara/cucumber"
 require "Faker"
-require 'mongo'
+require "mongo"
 
 Capybara.configure do |config|
-    config.default_driver = :selenium_chrome
-    config.app_host = "http://rocklov-web:3000"
-    config.default_max_wait_time = 10
+  config.default_driver = :selenium_chrome
+  config.app_host = "http://rocklov-web:3000"
+  config.default_max_wait_time = 10
 end
