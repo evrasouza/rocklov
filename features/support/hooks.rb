9b966770d5a2939e49a9f160b879870680511825
Everton Souza <evrasouza@gmail.com>
2021-03-29 20:00:03 -0300

    Redimensionando a janela para o teste

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -4,4 +4,7 @@ Before do
   @signup_page = SignUpPage.new
   @dash_page = DashPage.new
   @equipos_page = EquiposPage.new
+
+  #page.driver.browser.manage.window.maximize
+  page.current_window.resize_to(1440, 900)
 end
