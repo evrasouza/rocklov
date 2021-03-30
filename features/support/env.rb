7640c53b88e1c7c74f64ea813796847b7cfa1ec0
Everton Souza <evrasouza@gmail.com>
2021-03-30 19:11:02 -0300

    Allure reports com evidencias

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,3 +1,4 @@
+require "allure-cucumber"
 require "webdrivers"
 require "selenium-webdriver"
 require "capybara"
@@ -9,4 +10,9 @@ Capybara.configure do |config|
   config.default_driver = :selenium_chrome
   config.app_host = "http://rocklov-web:3000"
   config.default_max_wait_time = 10
+end
+
+AllureCucumber.configure do |config|
+  config.results_directory = "/logs"
+  config.clean_results_directory = true
 end
