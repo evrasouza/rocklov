ae2150543eea13e0305dbe1d05e9421d1e4f5f23
Everton Souza <evrasouza@gmail.com>
2021-03-30 21:02:51 -0300

    Contextos do RSpec

--- api/spec/post_session_spec.rb
+++ api/spec/post_session_spec.rb
@@ -1,24 +1,31 @@
 require "httparty"
 
 describe "POST/sessions" do
-  it "login com sucesso" do
-    payload = { "email": "everton@gmail.com", "password": "pwd123" }
+  context "login com sucesso" do
+    before(:all) do
+      payload = { "email": "everton@gmail.com", "password": "pwd123" }
 
-    result = HTTParty.post(
-      "http://rocklov-api:3333/sessions",
-      body: payload.to_json,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    )
+      @result = HTTParty.post(
+        "http://rocklov-api:3333/sessions",
+        body: payload.to_json,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      )
+    end
 
-    expect(result.code).to eql 200
-    expect(result.parsed_response["_id"].length).to eql 24
+    it "valida status code" do
+      expect(@result.code).to eql 200
+    end
 
-    #puts result
-    #puts result.class
-    #puts result.parsed_response
-    #puts result.parsed_response.class
-    #puts result.parsed_response["_id"]
+    it "valida id do usuário" do
+      expect(@result.parsed_response["_id"].length).to eql 24
+    end
   end
 end
+
+#puts result
+#puts result.class
+#puts result.parsed_response
+#puts result.parsed_response.class
+#puts result.parsed_response["_id"]
