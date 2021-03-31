3864f3842527e8fa70a5235d257f144ed9835398
Everton Souza <evrasouza@gmail.com>
2021-03-31 19:33:40 -0300

    Usuário Duplicado + Desafio

--- api/spec/post_signup_spec.rb
+++ api/spec/post_signup_spec.rb
@@ -1,5 +1,6 @@
 require_relative "routes/signup"
 require_relative "libs/mongo"
+require_relative "helpers"
 
 describe "POST/signup" do
   context "Novo Usuario" do
@@ -16,6 +17,45 @@ describe "POST/signup" do
 
     it "valida id do usuário" do
       expect(@result.parsed_response["_id"].length).to eql 24
+    end
+  end
+
+  context "Usuario ja Existe" do
+    before(:all) do
+      payload = { name: "Peter Gabriel", email: "peter@gmail.com", password: "pwd123" }
+      MongoDB.new.remove_user(payload[:email])
+      Signup.new.create(payload)
+      @result = Signup.new.create(payload)
+    end
+
+    it "deve retornar 409" do
+      expect(@result.code).to eql 409
+    end
+
+    it "valida mensagem de erro: Email already exists :(" do
+      expect(@result.parsed_response["error"]).to eql "Email already exists :("
+    end
+  end
+
+  #name
+  #email
+  #password
+
+  examples = Helpers::get_fixture("user")
+
+  examples.each do |e|
+    context "#{e[:title]}" do
+      before(:all) do
+        @result = Signup.new.create(e[:payload])
+      end
+
+      it "valida status code #{e[:code]}" do
+        expect(@result.code).to eql e[:code]
+      end
+
+      it "valida id do usuário #{e[:error]}" do
+        expect(@result.parsed_response["error"]).to eql e[:error]
+      end
     end
   end
 end
