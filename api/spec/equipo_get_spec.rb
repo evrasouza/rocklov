fba00fadabb6d091fda5b5ce9776d9004459a7ca
Everton Souza <evrasouza@gmail.com>
2021-03-31 22:11:44 -0300

    Automatizando GET Equipo

---
+++ api/spec/equipo_get_spec.rb
@@ -0,0 +1,36 @@
+describe "GET/equipos/{equipo_id}" do
+  before(:all) do
+    payload = { email: "everton@gmail.com", password: "pwd123" }
+    result = Sessions.new.login(payload)
+    @user_id = result.parsed_response["_id"]
+  end
+
+  context "Obter unico Equipo" do
+    before(:all) do
+
+      #dado que eu tenha um novo equipamento
+      @payload = {
+        thumbnail: Helpers::get_thumb("slash.jpg"),
+        name: "Guitarra do Slash",
+        category: "Cordas",
+        price: 250,
+      }
+      MongoDB.new.remove_equipo(@payload[:name], @user_id)
+
+      # e eu tenho o id deste equipamento
+      equipo = Equipos.new.create(@payload, @user_id)
+      @equipo_id = equipo.parsed_response["_id"]
+
+      #Quando faço uma requisição get por id
+      @result = Equipos.new.findd_by_id(@equipo_id, @user_id)
+    end
+
+    it "Deve retornar 200" do
+      expect(@result.code).to eql 200
+    end
+
+    it "Deve retornar o nome" do
+      expect(@result.parsed_response).to include("name" => @payload[:name])
+    end
+  end
+end
